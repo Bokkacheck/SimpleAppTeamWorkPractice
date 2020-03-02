1bd2ff8dda8b92b19bf53e8b956e48f1578e50da
Milovan <srejicmilovan@gmail.com>
2020-03-02 22:06:22 +0100

    Izmena Csharp

--- ClientApp/ClientApp/Form1.Designer.cs
+++ ClientApp/ClientApp/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.btnPrikaz = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -50,11 +51,22 @@
             this.btnPrikaz.UseVisualStyleBackColor = true;
             this.btnPrikaz.Click += new System.EventHandler(this.btnPrikaz_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(468, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 70);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Prikaz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_ClickAsync);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnPrikaz);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
@@ -67,6 +79,7 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button btnPrikaz;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ClientApp/ClientApp/Form1.cs
+++ ClientApp/ClientApp/Form1.cs
@@ -24,13 +24,43 @@ namespace ClientApp
         private void btnPrikaz_Click(object sender, EventArgs e)
         {
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://192.168.1.11:5000");
+            client.BaseAddress = new Uri("http://localhost:5000");
             HttpResponseMessage responseMessage = client.GetAsync("api/getPeople").Result;
             var people = responseMessage.Content.ReadAsStringAsync().Result;
             var peoples = JsonConvert.DeserializeObject<List<People>>(people,podesavanjeJson);
             foreach(var p in peoples)
             {
                 listBox1.Items.Add(p.firstName + " " + p.lastName);
+            }
+        }
+
+        private void Button1_ClickAsync(object sender, EventArgs e)
+        {
+            PostRequest("http://localhost:5000/newPerson");
+        }
+
+        private async void PostRequest(string url)
+        {
+            IEnumerable<KeyValuePair<string, string>> queries = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("id","3"),
+                new KeyValuePair<string, string>("firstName","Milovan"),
+                new KeyValuePair<string, string>("lastName","Srejic"),
+                new KeyValuePair<string, string>("age","21")
+            };
+            HttpContent q = new FormUrlEncodedContent(queries);
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.PostAsync(url, q))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+                        HttpContentHeaders headers = content.Headers;
+
+                        MessageBox.Show(mycontent);
+                    }
+                }
             }
         }
     }
