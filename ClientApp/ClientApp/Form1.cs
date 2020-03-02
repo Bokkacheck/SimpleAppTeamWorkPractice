4fec85fd0d669e9da7e051d5bc1379a52479fd65
Bokkacheck <bokis97@live.com>
2020-03-02 14:57:16 +0100

    Dodata KlijentApp
    dodato

---
+++ ClientApp/ClientApp/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+
+namespace ClientApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public JsonSerializerSettings podesavanjeJson = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto, Formatting = Formatting.Indented };
+        private void btnPrikaz_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://192.168.1.11:5000");
+            HttpResponseMessage responseMessage = client.GetAsync("api/getPeople").Result;
+            var people = responseMessage.Content.ReadAsStringAsync().Result;
+            var peoples = JsonConvert.DeserializeObject<List<People>>(people,podesavanjeJson);
+            foreach(var p in peoples)
+            {
+                listBox1.Items.Add(p.firstName + " " + p.lastName);
+            }
+        }
+    }
+}
---
+++ ClientApp/ClientApp/People.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class People
+    {
+        public int id;
+        public string firstName;
+        public string lastName;
+        public int age;
+
+        public People(int id, string firstName, string lastName, int age)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
